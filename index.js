477ea28a45e8cc6b1cff0d9493260444827164d2
Smokeybro916 <kyleluangasa@gmail.com>
2024-09-02 12:34:46 -0400

    added basic logout funtion

--- index.js
+++ index.js
@@ -54,11 +54,16 @@ app.post('/login', async (req,res) => {
   }
 })
 
+app.post('/logout', (req, res) => {
+  req.session.user_id = null;
+  res.redirect('/login');
+})
+
 app.get('/secret', (req, res) => {
   if(!req.session.user_id){
-    res.redirect('/login')
+    return res.redirect('/login')
   }
-  res.send('This is secret!')
+  res.render('secret')
 })
 
 app.listen(3000, () => {
